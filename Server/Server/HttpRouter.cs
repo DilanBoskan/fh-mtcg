4fb822d57a7affc80480eaa1226e4739d3cc55cf
Dilan Boskan <boskan.dilan@gmail.com>
2024-01-04 16:42:35 +0100

    Update line breaks

--- Server/Server/HttpRouter.cs
+++ Server/Server/HttpRouter.cs
@@ -48,15 +48,26 @@ public class HttpRouter : IHttpRouter {
     private HttpRequestHandler? GetHandler(HttpRequest request) {
         return request switch {
             // Users
-            { Method: "POST", Url: "/users" } => _userController.RegisterAsync, { Method: "GET" } when Regex.IsMatch(request.Url, @"/users/\w+") => _userController.GetAsync, { Method: "PUT" } when Regex.IsMatch(request.Url, @"/users/\w+") => _userController.UpdateAsync, { Method: "POST", Url: "/sessions" } => _userController.LoginAsync,
+            { Method: "POST", Url: "/users" } => _userController.RegisterAsync,
+            { Method: "GET" } when Regex.IsMatch(request.Url, @"/users/\w+") => _userController.GetAsync,
+            { Method: "PUT" } when Regex.IsMatch(request.Url, @"/users/\w+") => _userController.UpdateAsync,
+            { Method: "POST", Url: "/sessions" } => _userController.LoginAsync,
             // Packages
-            { Method: "POST", Url: "/packages" } => _packagesController.CreateAsync, { Method: "POST", Url: "/transactions/packages" } => _packagesController.AcquireAsync,
+            { Method: "POST", Url: "/packages" } => _packagesController.CreateAsync,
+            { Method: "POST", Url: "/transactions/packages" } => _packagesController.AcquireAsync,
             // Cards
-            { Method: "GET", Url: "/cards" } => _cardsController.GetAsync, { Method: "GET" } when Regex.IsMatch(request.Url, @"/deck") => _cardsController.GetDeckAsync, { Method: "PUT", Url: "/deck" } => _cardsController.SetDeckAsync,
+            { Method: "GET", Url: "/cards" } => _cardsController.GetAsync,
+            { Method: "GET" } when Regex.IsMatch(request.Url, @"/deck") => _cardsController.GetDeckAsync,
+            { Method: "PUT", Url: "/deck" } => _cardsController.SetDeckAsync,
             // Game
-            { Method: "GET", Url: "/stats" } => _gameController.GetStatsAsync, { Method: "GET", Url: "/scoreboard" } => _gameController.GetScoreboardAsync, { Method: "POST", Url: "/battles" } => _gameController.EnterBattleAsync,
+            { Method: "GET", Url: "/stats" } => _gameController.GetStatsAsync,
+            { Method: "GET", Url: "/scoreboard" } => _gameController.GetScoreboardAsync,
+            { Method: "POST", Url: "/battles" } => _gameController.EnterBattleAsync,
             // Trading
-            { Method: "GET", Url: "/tradings" } => _tradingController.GetAsync, { Method: "POST", Url: "/tradings" } => _tradingController.CreateAsync, { Method: "DELETE" } when Regex.IsMatch(request.Url, @"/tradings/\w+") => _tradingController.DeleteAsync, { Method: "POST" } when Regex.IsMatch(request.Url, @"/tradings/\w+") => _tradingController.TradeAsync,
+            { Method: "GET", Url: "/tradings" } => _tradingController.GetAsync,
+            { Method: "POST", Url: "/tradings" } => _tradingController.CreateAsync,
+            { Method: "DELETE" } when Regex.IsMatch(request.Url, @"/tradings/\w+") => _tradingController.DeleteAsync,
+            { Method: "POST" } when Regex.IsMatch(request.Url, @"/tradings/\w+") => _tradingController.TradeAsync,
 
             _ => null,
         };
